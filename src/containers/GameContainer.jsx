49b6071e7430f23f01939b32be98a67fa875a457
Scott Johnston <scott@umachan.co.uk>
2021-08-14 17:02:29 +0100

    Fixed check on click for side indexes

--- src/containers/GameContainer.jsx
+++ src/containers/GameContainer.jsx
@@ -12,23 +12,31 @@ function shuffleArray(array) {
 const GameContainer = () => {
     // const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; // 4 x 4 board for 15 tiles and 1 blank
 
-    // function shuffleArray(array) {
-    //     for (let i = array.length - 1; i > 0; i--) {
-    //         const j = Math.floor(Math.random() * (i + 1));
-    //         [array[i], array[j]] = [array[j], array[i]];
-    //     }
-    //     return array;
-    // }
-
     // State and variables
 
     const [winningBoard, setWinningBoard] = useState([
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        " ",
     ]);
 
-    const [gameBoard, setGameBoard] = useState(winningBoard);
+    const [gameBoard, setGameBoard] = useState([...winningBoard]);
 
-    const [tileSpace, setTileSpace] = useState(gameBoard.indexOf(16) + 1);
+    // const [tileSpace, setTileSpace] = useState(gameBoard.indexOf(16) + 1);
+    const [tileSpace, setTileSpace] = useState(gameBoard.indexOf(" ") + 1);
     console.log("Current tile space is: ", tileSpace);
     // const gameBoardSize = gameBoard.length;
 
@@ -42,6 +50,15 @@ const GameContainer = () => {
         return arr;
     };
 
+    const gameWon = (a, b) => {
+        return (
+            Array.isArray(a) &&
+            Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index])
+        );
+    };
+
     const handleTileClick = (tileNumberClicked) => {
         tileNumberClicked = gameBoard.indexOf(tileNumberClicked) + 1;
 
@@ -51,19 +68,14 @@ const GameContainer = () => {
 
         // Check if tile clicked has the space tile next to it defined as a space before, space after, space above or space below
         if (
-            tileNumberClicked + 1 === tileSpace ||
-            tileNumberClicked - 1 === tileSpace ||
+            (tileNumberClicked + 1 === tileSpace &&
+                tileNumberClicked % 4 !== 0) ||
+            (tileNumberClicked - 1 === tileSpace && tileSpace % 4 !== 0) ||
             tileNumberClicked + 4 === tileSpace ||
             tileNumberClicked - 4 === tileSpace
         ) {
-            console.log(
-                "Valid move: ",
-                gameBoard,
-                " : ",
-                tileSpace,
-                " : ",
-                tileNumberClicked
-            );
+            console.log("Valid move");
+
             setGameBoard(
                 swapArrayElements(
                     gameBoard,
@@ -71,13 +83,24 @@ const GameContainer = () => {
                     tileNumberClicked - 1
                 )
             );
-            // const tempTileSpace = tileSpace; // 16
-            // const tempTileNumberClicked = tileNumberClicked;
+
             setTileSpace(tileNumberClicked); // 12
-            // gameBoard[tileNumberClicked - 1] = tempTileSpace;
-            // gameBoard[tileNumberClicked - 4] = tempTileNumberClicked;
         } else {
             console.log("WRONG MOVE!! ");
+        }
+        console.log("gameBoard", gameBoard);
+        console.log("winningBoard", winningBoard);
+        console.log(
+            "tilespace ",
+            tileSpace,
+            " : tile number clicked",
+            tileNumberClicked
+        );
+
+        // Check if game won
+        if (gameWon(winningBoard, gameBoard)) {
+            alert("Game won!");
+            // Set a flag here so game cannot continue or button to start another game
         }
     };
 
